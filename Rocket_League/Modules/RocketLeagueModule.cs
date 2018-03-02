580e01eda4e2af3885c203ff1a1e5793f739e209
Wes Taylor <wesley.taylor303@gmail.com>
2018-03-01 23:13:17 -0500

    Small changes to the output of the builder. Working towards actual useful information output.

--- Rocket_League/Modules/RocketLeagueModule.cs
+++ Rocket_League/Modules/RocketLeagueModule.cs
@@ -15,41 +15,41 @@ namespace Rocket_League.Modules
     public class RocketLeagueModule : ModuleBase
     {
         [Command("rl")] //how to actually call the bot to do something
-        [Remarks("")]
+        [Remarks("Basic command to retrieve some kind of info from RL Stats")]
         public async Task singlePlayerStats([Remainder] string RlUsernameInputtedByUser)
-        {
+        {   //create the builder to output into the Discord chat window
             var builder = new EmbedBuilder()
             {
                 Color = new Color(100, 65, 200),
                 Description = "\n"
 
             };
+
             Services.ConfigurationService RLconfig = new Services.ConfigurationService();
+
             var apiKey = RLconfig.GetRLapiKey();
 
             var client = new RLSClient(apiKey);
-            // Retrieve a single player.
+
+            // Retrieve a single player on PS4
             var player = await client.GetPlayerAsync(RlsPlatform.Ps4, RlUsernameInputtedByUser);
-            var playerSeasonSix = player.RankedSeasons.FirstOrDefault(x => x.Key == RlsSeason.Six);
+            var playerSeasonSeven = player.RankedSeasons.FirstOrDefault(x => x.Key == RlsSeason.Seven);
 
-            if (playerSeasonSix.Value != null)
+            if (playerSeasonSeven.Value != null)
             {
-                builder.Description = builder.Description + "\n" + $"# Player: {player.DisplayName}";
+                builder.Description = builder.Description + "\n" + $"Display Name: {player.DisplayName}";
 
-                foreach (var playerRank in playerSeasonSix.Value)
+                foreach (var playerRank in playerSeasonSeven.Value)
                 {
                     builder.Description = builder.Description + "\n" + $"{playerRank.Key}: {playerRank.Value.RankPoints} rating";
                 }
+
+                builder.Description = builder.Description + "\n" + "Platform: " + player.Platform.Name;
+                builder.Description = builder.Description + "\n" + "wins: " + player.Stats.Wins;
+                
             }          
             
             await ReplyAsync("", false,builder.Build());
-        }
-
-        [Command("rl2")] //how to actually call the bot to do something
-        [Remarks("")]
-        public async Task singlePlayerStats([Remainder] string RlUsernameInputtedByUser)
-        {
-        }
-                
+        }           
     }
 }
